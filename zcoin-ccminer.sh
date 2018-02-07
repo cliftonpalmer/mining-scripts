53690ab612a4c8d4532a534befe1048c5cceb80f
Clifton Palmer <clifton.palmer@gmail.com>
2018-02-07 13:30:20 -0600

    Reformatted zcoin miner

--- zcoin-ccminer.sh
+++ zcoin-ccminer.sh
@@ -2,4 +2,7 @@
 
 wallet=$1
 
-ccminer -a lyra2z -o stratum+tcp://pool.datapaw.net:4553 -u $wallet -p c=XZC,`hostname`
+ccminer -a lyra2z \
+	-o stratum+tcp://us-east.zcoin.miningpoolhub.com:20581 \
+	-u sassylizard.miner1 \
+	-p thanks
