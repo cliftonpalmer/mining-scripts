9bd94c9111e8c08ff175daca7e14551ebd99443d
Clifton Palmer <clifton.palmer@gmail.com>
2018-02-11 11:21:21 -0600

    Fixed up multi algo mining

--- multi-algo.sh
+++ multi-algo.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 
 username=$1
-workername=$2
-password=$3
-
-stratum_farm_uri=us-east.equihash-hub.miningpoolhub.com:12023
+workername=`hostname`
+password=$2
 
 # run the miner in a pool
 # from https://miningpoolhub.com/?page=gettingstarted
-
 while true
 do
-	# -U: use CUDA
-	# -G: use OpenGL
-	# -S: stratum server:port
-	# -O: stratum credentials
-	# -FS: failover
-	ethminer --farm-retries 0 \
-		-G \
-		-S "$stratum_farm_uri" \
-		-O "$username.$workername:$password" \
-		-FS exit
-
-	break
+	ethminer -U -S us-east.ethash-hub.miningpoolhub.com:12020 -O $username.$workername:"$password" -FS exit
+	bminer -uri stratum+ssl://$username.$workername@us-east.equihash-hub.miningpoolhub.com:12023 -max-network-failures=0 -watchdog=false
 	ccminer -r 0 -a groestl -o stratum+tcp://hub.miningpoolhub.com:12004 -u $username.$workername -p "$password"
 	ccminer -r 0 -a myr-gr -o stratum+tcp://hub.miningpoolhub.com:12005 -u $username.$workername -p "$password"
 	ccminer -r 0 -a x11 -o stratum+tcp://hub.miningpoolhub.com:12007 -u $username.$workername -p "$password"
@@ -35,6 +22,6 @@ do
 	ccminer -r 0 -a lyra2v2 -o stratum+tcp://hub.miningpoolhub.com:12018 -u $username.$workername -p "$password"
 	ccminer -r 0 -a vanilla -o stratum+tcp://hub.miningpoolhub.com:12019 -u $username.$workername -p "$password"
 
-	echo Done with an interation, sleeping
-	sleep 10
+	echo Done with an interation
+	sleep 1
 done
